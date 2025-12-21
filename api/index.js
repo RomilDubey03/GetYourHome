d9879224f5a43bb0a077fae010f47e0ff3dc5b71
Romil Dubey <romildubey03@gmail.com>
2025-12-21 21:19:31 +0530

    vercel config

--- api/index.js
+++ api/index.js
@@ -1,52 +1,54 @@
-import express from 'express'
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import mongoose  from 'mongoose';
-import UserRouter from './routes/user.route.js'
-import AuthRouter from './routes/auth.route.js'
-import listingRouter from './routes/listing.route.js';
-import BookingRouter from './routes/booking.route.js'
-import cookieParser from 'cookie-parser';
-import path from 'path';
+import mongoose from "mongoose";
+import cors from "cors";
+import UserRouter from "./routes/user.route.js";
+import AuthRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import BookingRouter from "./routes/booking.route.js";
+import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 3000;
 
-main().catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
 
-async function main() {
-  await mongoose.connect(process.env.MONGO_URL);
-    console.log("database connected");
-}
-const app = express()
+const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "https://get-your-home.vercel.app",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use("/api/user", UserRouter);
+app.use("/api/auth", AuthRouter);
+app.use("/api/listing", listingRouter);
+app.use("/api/booking", BookingRouter);
 
-app.use(express.json())
-app.use(cookieParser())
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "kuch to gadbad he daya";
 
-app.use('/api/user' , UserRouter)
-app.use('/api/auth' , AuthRouter)
-app.use('/api/listing' , listingRouter)
-app.use('/api/booking' , BookingRouter)
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+app.listen(PORT, () => {
+  console.log(`server running at : http://localhost:${PORT}`);
+});
 
-app.use((err , req , res , next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "kuch to gadbad he daya";
-
-    return res.status(statusCode).json({
-        success : false,
-        statusCode,
-        message
-    })
-})
-
-app.listen(PORT , ()=>{
-    console.log(`server running at : http://localhost:${PORT}`);
-})
+export default app;
